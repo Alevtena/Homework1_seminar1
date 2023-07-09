a3f0e1ea5255e9dd1c635529f366e6bb29d2f49a
Alevtena <success_2006@mail.ru>
2023-07-09 13:49:52 +0300

    Added the task number 4

--- Program.cs
+++ Program.cs
@@ -33,8 +33,21 @@ Console.WriteLine ("Max number is: " + max);
 // -3 -> нет
 // 7 -> нет
 
+/*
 Console.Write ("Input any number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int even = 2;
 if (num % even ==0) Console.WriteLine ($"Number {num} is even");
 else Console.WriteLine ($"Number {num} is not even");
+*/
+
+// Task 4.  программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write ("Input any number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i <= N; i++) 
+{
+    if (i % 2 == 0) Console.Write (i + " ");
+}
