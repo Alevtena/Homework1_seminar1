275ce4dbeac948fc66dabe0cb696ce7b53e38abc
Alevtena <success_2006@mail.ru>
2023-07-09 13:13:56 +0300

    Added the task number 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт большее число.
+﻿// Task 1. Напишите программу, которая на вход принимает два числа и выдаёт большее число.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
+/*
 Console.Write ("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write ("Input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2) Console.WriteLine ("Max number is: " + num1);
-else Console.WriteLine ("Max number is: " + num2);
+else Console.WriteLine ("Max number is: " + num2); 
+*/
+
+// Task 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write ("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3; 
+Console.WriteLine ("Max number is: " + max);
