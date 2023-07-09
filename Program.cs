e0ae71519b8dfaeb49f5cb91dc82120a7b7d17be
Alevtena <success_2006@mail.ru>
2023-07-09 13:25:11 +0300

    Added the task number 3

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ else Console.WriteLine ("Max number is: " + num2);
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-
+/*
 Console.Write ("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write ("Input a second number: ");
@@ -26,3 +26,15 @@ int max = num1;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3; 
 Console.WriteLine ("Max number is: " + max);
+*/
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write ("Input any number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+if (num % even ==0) Console.WriteLine ($"Number {num} is even");
+else Console.WriteLine ($"Number {num} is not even");
